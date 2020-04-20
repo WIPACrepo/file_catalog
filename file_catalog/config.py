779c195e5da2888766a475898d4503bd94a5e91e
Ric Evans <emejqz@gmail.com>
2020-04-20 17:53:55 -0500

    change MONGOD_URI to MONGODB_URI

--- file_catalog/config.py
+++ file_catalog/config.py
@@ -29,7 +29,7 @@ class Config(dict):
                 'MongoDB host'),
         'MONGODB_PORT': cps(27017, int,
                 'MongoDB port'),
-        'MONGOD_URI': cps(None, str,
+        'MONGODB_URI': cps(None, str,
                 'MongoDB URI'),
         'META_FORBIDDEN_FIELDS_CREATION': cps(
                 ['mongo_id', '_id', 'meta_modify_date'], str.split,

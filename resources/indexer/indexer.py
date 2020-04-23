57489bf1454ab8bca3889d474dbaf21682606cef
Ric Evans <emejqz@gmail.com>
2020-04-23 17:01:25 -0500

    make parsing functions static, easier to test

--- resources/indexer/indexer.py
+++ resources/indexer/indexer.py
@@ -144,15 +144,12 @@ class BasicFileMetadata:
 class I3FileMetadata(BasicFileMetadata):
     """Metadata for i3 files"""
 
-    def __init__(self, file, site, processing_level):
+    def __init__(self, file, site, processing_level, filename_formats):
         super().__init__(file, site)
         self.processing_level = processing_level
-        self.season_year = None
-        self.run = 0
-        self.subrun = 0
-        self.part = 0
         self.meta_xml = {}
-        self._parse_filepath()
+        self.season_year, self.run, self.subrun, self.part = I3FileMetadata.parse_year_run_subrun_part(
+            filename_formats, self.file.name)
 
     def generate(self):
         """Gather the file's metadata."""
@@ -181,34 +178,31 @@ class I3FileMetadata(BasicFileMetadata):
             }
         return metadata
 
-    def _parse_filepath(self):
-        """Set the year, run, subrun, and part from the file name."""
-        raise NotImplementedError()
-
-    def _assign_year_run_subrun_part(self, formats):
-        """Set the year, run, subrun, and part from the file name from regex named groups."""
+    @staticmethod
+    def parse_year_run_subrun_part(formats, filename):
+        """Return the year, run, subrun, and part from the file name from regex named groups."""
         values = {'year': None, 'run': 0, 'subrun': 0, 'part': 0}
 
         for pattern in formats:
-            match = re.match(pattern, self.file.name)
+            match = re.match(pattern, filename)
             if match:
                 values.update(match.groupdict())
                 if 'ic_strings' in values:
-                    self.season_year = IceCubeSeason.name_to_year(f"IC{values['ic_strings']}")
+                    year = IceCubeSeason.name_to_year(f"IC{values['ic_strings']}")
                 elif values['year']:
-                    self.season_year = values['year']
-                self.run = int(values['run'])
-                self.subrun = int(values['subrun'])
-                self.part = int(values['part'])
-                return
+                    year = values['year']
+                run = int(values['run'])
+                subrun = int(values['subrun'])
+                part = int(values['part'])
+                return year, run, subrun, part
 
-        raise Exception(f"Filename not in a known {self.processing_level} file format, {self.file.name}.")
+        raise Exception()
 
     @staticmethod
-    def parse_run_number(file):
+    def parse_run_number(filename):
         """Return run number from filename."""
         # Ex: Run00125791_GapsTxt.tar
-        match = re.match(r'(.*)Run(?P<run>\d+)', file.name)
+        match = re.match(r'(.*)Run(?P<run>\d+)', filename)
         run = match.groupdict()['run']
         return run
 
@@ -319,8 +313,33 @@ class I3FileMetadata(BasicFileMetadata):
 class L2FileMetadata(I3FileMetadata):
     """Metadata for L2 i3 files"""
 
+    FILENAME_FORMATS = [
+        # Ex: Level2_IC86.2017_data_Run00130567_Subrun00000000_00000280.i3.zst
+        # Ex: Level2pass2_IC79.2010_data_Run00115975_Subrun00000000_00000055.i3.zst
+        r'(.*)(\.20\d{2})_data_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
+
+        # Ex: Level2_PhysicsTrig_PhysicsFiltering_Run00120374_Subrun00000000_00000001.i3
+        # Ex: Level2pass3_PhysicsFiltering_Run00127353_Subrun00000000_00000000.i3.gz
+        r'(.*)_PhysicsFiltering_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
+
+        # Ex: Level2_IC86.2016_data_Run00129004_Subrun00000316.i3.bz2
+        # Ex: Level2_IC86.2012_Test_data_Run00120028_Subrun00000081.i3.bz2
+        # Ex: Level2_IC86.2015_24HrTestRuns_data_Run00126291_Subrun00000203.i3.bz2
+        r'(.*)\.(?P<year>20\d{2})_data_Run(?P<run>\d+)_Subrun(?P<part>\d+)(.*)',
+
+        # Ex: Level2_IC86.2011_data_Run00119221_Part00000126.i3.bz2
+        r'(.*)\.(?P<year>20\d{2})_data_Run(?P<run>\d+)_Part(?P<part>\d+)(.*)',
+
+        # Ex: Level2a_IC59_data_Run00115968_Part00000290.i3.gz
+        # Ex: MoonEvents_Level2_IC79_data_Run00116082_NewPart00000613.i3.gz
+        r'(.*)_IC(?P<ic_strings>\d+)_data_Run(?P<run>\d+)_(New)?Part(?P<part>\d+)(.*)',
+
+        # Ex: Level2_All_Run00111562_Part00000046.i3.gz
+        r'(.*)_All_Run(?P<run>\d+)(.*)_Part(?P<part>\d+)(.*)'
+    ]
+
     def __init__(self, file, site, dir_meta_xml, gaps_dict, gcd_filepath):
-        super().__init__(file, site, ProcessingLevel.L2)
+        super().__init__(file, site, ProcessingLevel.L2, L2FileMetadata.FILENAME_FORMATS)
         self.meta_xml = dir_meta_xml
         self.gaps_dict = gaps_dict
         self.gcd_filepath = gcd_filepath
@@ -383,35 +402,6 @@ class L2FileMetadata(I3FileMetadata):
         }
         return metadata
 
-    def _parse_filepath(self):
-        """Set the year, run, subrun, and part from the file name."""
-        formats = [
-            # Ex: Level2_IC86.2017_data_Run00130567_Subrun00000000_00000280.i3.zst
-            # Ex: Level2pass2_IC79.2010_data_Run00115975_Subrun00000000_00000055.i3.zst
-            r'(.*)(\.20\d{2})_data_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
-
-            # Ex: Level2_PhysicsTrig_PhysicsFiltering_Run00120374_Subrun00000000_00000001.i3
-            # Ex: Level2pass3_PhysicsFiltering_Run00127353_Subrun00000000_00000000.i3.gz
-            r'(.*)_PhysicsFiltering_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
-
-            # Ex: Level2_IC86.2016_data_Run00129004_Subrun00000316.i3.bz2
-            # Ex: Level2_IC86.2012_Test_data_Run00120028_Subrun00000081.i3.bz2
-            # Ex: Level2_IC86.2015_24HrTestRuns_data_Run00126291_Subrun00000203.i3.bz2
-            r'(.*)\.(?P<year>20\d{2})_data_Run(?P<run>\d+)_Subrun(?P<part>\d+)(.*)',
-
-            # Ex: Level2_IC86.2011_data_Run00119221_Part00000126.i3.bz2
-            r'(.*)\.(?P<year>20\d{2})_data_Run(?P<run>\d+)_Part(?P<part>\d+)(.*)',
-
-            # Ex: Level2a_IC59_data_Run00115968_Part00000290.i3.gz
-            # Ex: MoonEvents_Level2_IC79_data_Run00116082_NewPart00000613.i3.gz
-            r'(.*)_IC(?P<ic_strings>\d+)_data_Run(?P<run>\d+)_(New)?Part(?P<part>\d+)(.*)',
-
-            # Ex: Level2_All_Run00111562_Part00000046.i3.gz
-            r'(.*)_All_Run(?P<run>\d+)(.*)_Part(?P<part>\d+)(.*)'
-        ]
-
-        self._assign_year_run_subrun_part(formats)
-
     @staticmethod
     def is_file(file, processing_level):
         """True if the file is in the [...#].i3[...] file format."""
@@ -423,24 +413,20 @@ class L2FileMetadata(I3FileMetadata):
 class PFFiltFileMetadata(I3FileMetadata):
     """Metadata for PFFilt i3 files"""
 
+    FILENAME_FORMATS = [
+        # Ex: PFFilt_PhysicsFiltering_Run00131989_Subrun00000000_00000295.tar.bz2
+        # Ex: PFFilt_PhysicsTrig_PhysicsFiltering_Run00121503_Subrun00000000_00000314.tar.bz2
+        # Ex: orig.PFFilt_PhysicsFiltering_Run00127080_Subrun00000000_00000244.tar.bz2.orig
+        r'(.*)PFFilt_(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
+
+        # Ex: PFFilt_PhysicsTrig_PhysicsFilt_Run00089959_00180.tar.gz
+        # Ex: PFFilt_PhysicsTrig_RandomFilt_Run86885_006.tar.gz
+        r'PFFilt_(.*)_Run(?P<run>\d+)_(?P<part>\d+)(.*)'
+    ]
+
     def __init__(self, file, site):
-        super().__init__(file, site, ProcessingLevel.PFFilt)
+        super().__init__(file, site, ProcessingLevel.PFFilt, PFFiltFileMetadata.FILENAME_FORMATS)
         self._grab_meta_xml_from_tar()
-
-    def _parse_filepath(self):
-        """Set the year, run, subrun, and part from the file name."""
-        formats = [
-            # Ex: PFFilt_PhysicsFiltering_Run00131989_Subrun00000000_00000295.tar.bz2
-            # Ex: PFFilt_PhysicsTrig_PhysicsFiltering_Run00121503_Subrun00000000_00000314.tar.bz2
-            # Ex: orig.PFFilt_PhysicsFiltering_Run00127080_Subrun00000000_00000244.tar.bz2.orig
-            r'(.*)PFFilt_(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
-
-            # Ex: PFFilt_PhysicsTrig_PhysicsFilt_Run00089959_00180.tar.gz
-            # Ex: PFFilt_PhysicsTrig_RandomFilt_Run86885_006.tar.gz
-            r'PFFilt_(.*)_Run(?P<run>\d+)_(?P<part>\d+)(.*)'
-        ]
-
-        self._assign_year_run_subrun_part(formats)
 
     @staticmethod
     def is_file(file, processing_level):
@@ -452,26 +438,22 @@ class PFFiltFileMetadata(I3FileMetadata):
 class PFDSTFileMetadata(I3FileMetadata):
     """Metadata for PFDST i3 files"""
 
+    FILENAME_FORMATS = [
+        # Ex. ukey_fa818e64-f6d2-4cc1-9b34-e50bfd036bf3_PFDST_PhysicsFiltering_Run00131437_Subrun00000000_00000066.tar.gz
+        # Ex: ukey_42c89a63-e3f7-4c3e-94ae-840eff8bd4fd_PFDST_RandomFiltering_Run00131155_Subrun00000051_00000000.tar.gz
+        # Ex: PFDST_PhysicsFiltering_Run00125790_Subrun00000000_00000064.tar.gz
+        # Ex: PFDST_UW_PhysicsFiltering_Run00125832_Subrun00000000_00000000.tar.gz
+        # Ex: PFDST_RandomFiltering_Run00123917_Subrun00000000_00000000.tar.gz
+        # Ex: PFDST_PhysicsTrig_PhysicsFiltering_Run00121663_Subrun00000000_00000091.tar.gz
+        # Ex: PFDST_TestData_PhysicsFiltering_Run00122158_Subrun00000000_00000014.tar.gz
+        # Ex: PFDST_TestData_RandomFiltering_Run00119375_Subrun00000136_00000000.tar.gz
+        # Ex: PFDST_TestData_Unfiltered_Run00119982_Subrun00000000_000009.tar.gz
+        r'(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)'
+    ]
+
     def __init__(self, file, site):
-        super().__init__(file, site, ProcessingLevel.PFDST)
+        super().__init__(file, site, ProcessingLevel.PFDST, PFDSTFileMetadata.FILENAME_FORMATS)
         self._grab_meta_xml_from_tar()
-
-    def _parse_filepath(self):
-        """Set the year, run, subrun, and part from the file name."""
-        formats = [
-            # Ex. ukey_fa818e64-f6d2-4cc1-9b34-e50bfd036bf3_PFDST_PhysicsFiltering_Run00131437_Subrun00000000_00000066.tar.gz
-            # Ex: ukey_42c89a63-e3f7-4c3e-94ae-840eff8bd4fd_PFDST_RandomFiltering_Run00131155_Subrun00000051_00000000.tar.gz
-            # Ex: PFDST_PhysicsFiltering_Run00125790_Subrun00000000_00000064.tar.gz
-            # Ex: PFDST_UW_PhysicsFiltering_Run00125832_Subrun00000000_00000000.tar.gz
-            # Ex: PFDST_RandomFiltering_Run00123917_Subrun00000000_00000000.tar.gz
-            # Ex: PFDST_PhysicsTrig_PhysicsFiltering_Run00121663_Subrun00000000_00000091.tar.gz
-            # Ex: PFDST_TestData_PhysicsFiltering_Run00122158_Subrun00000000_00000014.tar.gz
-            # Ex: PFDST_TestData_RandomFiltering_Run00119375_Subrun00000136_00000000.tar.gz
-            # Ex: PFDST_TestData_Unfiltered_Run00119982_Subrun00000000_000009.tar.gz
-            r'(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)'
-        ]
-
-        self._assign_year_run_subrun_part(formats)
 
     @staticmethod
     def is_file(file, processing_level):
@@ -484,29 +466,25 @@ class PFDSTFileMetadata(I3FileMetadata):
 class PFRawFileMetadata(I3FileMetadata):
     """Metadata for PFRaw i3 files"""
 
+    FILENAME_FORMATS = [
+        # Ex: key_31445930_PFRaw_PhysicsFiltering_Run00128000_Subrun00000000_00000156.tar.gz
+        # Ex: ukey_b98a353f-72e8-4d2e-afd7-c41fa5c8d326_PFRaw_PhysicsFiltering_Run00131322_Subrun00000000_00000018.tar.gz
+        # Ex: ukey_05815dd9-2411-468c-9bd5-e99b8f759efd_PFRaw_RandomFiltering_Run00130470_Subrun00000060_00000000.tar.gz
+        # Ex: PFRaw_PhysicsTrig_PhysicsFiltering_Run00114085_Subrun00000000_00000208.tar.gz
+        # Ex: PFRaw_TestData_PhysicsFiltering_Run00114672_Subrun00000000_00000011.tar.gz
+        # Ex: PFRaw_TestData_RandomFiltering_Run00113816_Subrun00000033_00000000.tar.gz
+        r'(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
+
+        # Ex: EvtMonPFRaw_PhysicsTrig_RandomFiltering_Run00106489_Subrun00000000.tar.gz
+        r'(.*)_Run(?P<run>\d+)_Subrun(?P<part>\d+)(.*)',
+
+        # Ex: DebugData_PFRaw_Run110394_1.tar.gz
+        r'(.*)_Run(?P<run>\d+)_(?P<part>\d+)(.*)'
+    ]
+
     def __init__(self, file, site):
-        super().__init__(file, site, ProcessingLevel.PFRaw)
+        super().__init__(file, site, ProcessingLevel.PFRaw, PFRawFileMetadata.FILENAME_FORMATS)
         self._grab_meta_xml_from_tar()
-
-    def _parse_filepath(self):
-        """Set the year, run, subrun, and part from the file name."""
-        formats = [
-            # Ex: key_31445930_PFRaw_PhysicsFiltering_Run00128000_Subrun00000000_00000156.tar.gz
-            # Ex: ukey_b98a353f-72e8-4d2e-afd7-c41fa5c8d326_PFRaw_PhysicsFiltering_Run00131322_Subrun00000000_00000018.tar.gz
-            # Ex: ukey_05815dd9-2411-468c-9bd5-e99b8f759efd_PFRaw_RandomFiltering_Run00130470_Subrun00000060_00000000.tar.gz
-            # Ex: PFRaw_PhysicsTrig_PhysicsFiltering_Run00114085_Subrun00000000_00000208.tar.gz
-            # Ex: PFRaw_TestData_PhysicsFiltering_Run00114672_Subrun00000000_00000011.tar.gz
-            # Ex: PFRaw_TestData_RandomFiltering_Run00113816_Subrun00000033_00000000.tar.gz
-            r'(.*)_Run(?P<run>\d+)_Subrun(?P<subrun>\d+)_(?P<part>\d+)(.*)',
-
-            # Ex: EvtMonPFRaw_PhysicsTrig_RandomFiltering_Run00106489_Subrun00000000.tar.gz
-            r'(.*)_Run(?P<run>\d+)_Subrun(?P<part>\d+)(.*)',
-
-            # Ex: DebugData_PFRaw_Run110394_1.tar.gz
-            r'(.*)_Run(?P<run>\d+)_(?P<part>\d+)(.*)'
-        ]
-
-        self._assign_year_run_subrun_part(formats)
 
     @staticmethod
     def is_file(file, processing_level):
@@ -555,7 +533,7 @@ class MetadataManager:
                     pass
             # GCD Files (one per run)
             elif "GCD" in dir_entry.name:  # Ex. Level2_IC86.2017_data_Run00130484_0101_71_375_GCD.i3.zst
-                run = I3FileMetadata.parse_run_number(dir_entry)
+                run = I3FileMetadata.parse_run_number(dir_entry.name)
                 gcd_files[str(run)] = dir_entry.path
         self.l2_dir_metadata['dir_meta_xml'] = dir_meta_xml
         self.l2_dir_metadata['gaps_files'] = gaps_files
@@ -579,7 +557,7 @@ class MetadataManager:
                 except KeyError:
                     gaps = {}
                 try:
-                    run = I3FileMetadata.parse_run_number(file)
+                    run = I3FileMetadata.parse_run_number(file.name)
                     gcd = self.l2_dir_metadata['gcd_files'][str(run)]
                 except KeyError:
                     gcd = ""

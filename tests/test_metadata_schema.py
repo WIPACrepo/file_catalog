3d924fac3d32d0382f6f0957b0646f4a3795f7bf
Ric Evans <emejqz@gmail.com>
2021-03-01 11:54:46 -0600

    check for members in test_00_types.py

--- tests/test_metadata_schema.py
+++ tests/test_metadata_schema.py
@@ -1,12 +1,24 @@
 """Test metadata_schema."""
 
 
-from inspect import getmembers
-
 # local imports
 from file_catalog import metadata_schema
 
 
 def test_00_types() -> None:
     """Simply check imports."""
-    assert "Metadata" in getmembers(metadata_schema.types)
+    type_dicts = [
+        "Checksum",
+        "LocationEntry",
+        "SoftwareEntry",
+        "EventsData",
+        "Run",
+        "GapEntry",
+        "Event",
+        "OfflineProcessingMetadata",
+        "IceProdMetadata",
+        "SimulationMetadata",
+        "Metadata",
+    ]
+    for type_dict_class in type_dicts:
+        assert type_dict_class in dir(metadata_schema.types)

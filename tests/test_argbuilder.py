4dfcfedc1e3939e96503c4b3d2fe93b9d7efef4c
Ric Evans <emejqz@gmail.com>
2021-03-03 15:19:24 -0600

    remove needless variables

--- tests/test_argbuilder.py
+++ tests/test_argbuilder.py
@@ -15,12 +15,6 @@ def test_00_path_args() -> None:
         kwargs_in: Dict[str, Any]
         kwargs_after: Dict[str, Any]
 
-    path_regex = r"/reg-ex/this.*/(file/)?path"
-    path = "PATH"
-    logical_name = "LOGICAL_NAME"
-    directory = "/path/to/dir/"
-    filename = "my-file"
-
     kwargs_test_dicts: List[KwargsTests] = [
         # null case
         {"kwargs_in": {}, "kwargs_after": {}},
@@ -31,39 +25,39 @@ def test_00_path_args() -> None:
         },
         # only "path-regex"
         {
-            "kwargs_in": {"path-regex": path_regex},
-            "kwargs_after": {"logical_name": {"$regex": path_regex}},
+            "kwargs_in": {"path-regex": r"/reg-ex/this.*/(file/)?path"},
+            "kwargs_after": {
+                "logical_name": {"$regex": r"/reg-ex/this.*/(file/)?path"}
+            },
         },
         # only "path"
         {
-            "kwargs_in": {"an-extra-argument": [12, 34, 56], "path": path},
-            "kwargs_after": {"an-extra-argument": [12, 34, 56], "logical_name": path},
+            "kwargs_in": {"an-extra-argument": [12, 34, 56], "path": "PATH"},
+            "kwargs_after": {"an-extra-argument": [12, 34, 56], "logical_name": "PATH"},
         },
         # only "logical_name"
         {
-            "kwargs_in": {"logical_name": logical_name},
-            "kwargs_after": {"logical_name": logical_name},
+            "kwargs_in": {"logical_name": "LOGICAL_NAME"},
+            "kwargs_after": {"logical_name": "LOGICAL_NAME"},
         },
         # only "directory"
-        {
-            "kwargs_in": {"directory": directory},
-            "kwargs_after": {
-                "logical_name": {"$regex": rf"^{directory.rstrip('/')}((/)|(/.*/)).*$"},
-            },
-        },
-        # only "directory" w/o trailing '/'
         {
             "kwargs_in": {"directory": "/path/to/dir/"},
             "kwargs_after": {
                 "logical_name": {"$regex": r"^/path/to/dir((/)|(/.*/)).*$"},
             },
         },
+        # only "directory" w/o trailing '/'
+        {
+            "kwargs_in": {"directory": "/path/to/dir"},
+            "kwargs_after": {
+                "logical_name": {"$regex": r"^/path/to/dir((/)|(/.*/)).*$"},
+            },
+        },
         # only "filename"
         {
-            "kwargs_in": {"filename": filename},
-            "kwargs_after": {
-                "logical_name": {"$regex": rf"^.*((/)|(/.*/)){filename}$"}
-            },
+            "kwargs_in": {"filename": "my-file"},
+            "kwargs_after": {"logical_name": {"$regex": r"^.*((/)|(/.*/))my-file$"}},
         },
         # only "filename" w/ a sub-directory
         {
@@ -74,7 +68,7 @@ def test_00_path_args() -> None:
         },
         # "directory" & "filename"
         {
-            "kwargs_in": {"directory": directory, "filename": filename},
+            "kwargs_in": {"directory": "/path/to/dir/", "filename": "my-file"},
             "kwargs_after": {
                 "logical_name": {"$regex": r"^/path/to/dir((/)|(/.*/))my-file$"}
             },
@@ -89,11 +83,11 @@ def test_00_path_args() -> None:
 
     # test multiple path-args (each loop pops the arg of the highest precedence)
     args = [  # list in decreasing order of precedence
-        ("directory", directory, {"$regex": r"^/path/to/dir((/)|(/.*/)).*$"}),
+        ("directory", "/path/to/dir/", {"$regex": r"^/path/to/dir((/)|(/.*/)).*$"}),
         # not testing "filename" b/c that is equal to "directory" in precedence
-        ("logical_name", logical_name, logical_name),
-        ("path", path, path),
-        ("path-regex", path_regex, {"$regex": path_regex}),
+        ("logical_name", "LOGICAL_NAME", "LOGICAL_NAME"),
+        ("path", "PATH", "PATH"),
+        ("path-regex", r"this.*is?a.path", {"$regex": r"this.*is?a.path"}),
     ]
     while args:
         kwargs = {k: v for (k, v, _) in args}

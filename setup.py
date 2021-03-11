b4c1f4130325aead19b68d5912a8f5371d316060
Ric Evans <emejqz@gmail.com>
2021-03-11 17:55:46 -0600

    type-hint

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ from typing import List, Tuple
 from setuptools import setup  # type: ignore[import]
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-OLDEST_PY_VERSION = (3, 5)
-PY_VERSION = (3, 8)
+OLDEST_PY_VERSION: Tuple[int, int] = (3, 5)
+PY_VERSION: Tuple[int, int] = (3, 8)
 NAME = "file_catalog"
 REQUIREMENTS_PATH = os.path.join(HERE, "requirements.txt")
 REQUIREMENTS = open(REQUIREMENTS_PATH).read().splitlines()

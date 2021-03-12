db8bb4edce5d4b45c75235aac19d42d09cdc52a4
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2021-03-11 18:04:53 -0600

    Setup.py Updates & Boiler-plating (#92)
    * setup.py: make boilerplate-ready code; parse & validate git+https:* pips
    
    * tie python language classifiers to `PY_VERSION`
    
    * programmatically figure Development Status; define OLDEST_PY_VERSION
    
    * type-hint
    
    * OLDEST_PY_VERSION -> 3.6

--- setup.py
+++ setup.py
@@ -1,48 +1,110 @@
 """Setup."""
 
-# fmt:off
-# pylint: skip-file
 
-from os import path
+import os
+import re
+import sys
+from typing import List, Tuple
 
 from setuptools import setup  # type: ignore[import]
 
-# local imports
-import file_catalog
+HERE = os.path.abspath(os.path.dirname(__file__))
+OLDEST_PY_VERSION: Tuple[int, int] = (3, 6)
+PY_VERSION: Tuple[int, int] = (3, 8)
+NAME = "file_catalog"
+REQUIREMENTS_PATH = os.path.join(HERE, "requirements.txt")
+REQUIREMENTS = open(REQUIREMENTS_PATH).read().splitlines()
 
-here = path.abspath(path.dirname(__file__))
 
-long_description = open(path.join(here, 'README.md')).read()
-install_requires = [
-    m.strip().replace('==', '>=') for m in open(path.join(here, 'requirements.txt'))
-]
+# Check Python Version -----------------------------------------------------------------
+if sys.version_info < PY_VERSION:
+    print(
+        f"ERROR: {NAME} requires at least Python {PY_VERSION[0]}.{PY_VERSION[1]}+ to run."
+    )
+    sys.exit(1)
+
+
+# Helper Utilities ---------------------------------------------------------------------
+
+
+def _get_version() -> str:
+    with open(os.path.join(HERE, NAME, "__init__.py")) as init_f:
+        for line in init_f.readlines():
+            if "__version__" in line:
+                # grab "X.Y.Z" from "__version__ = 'X.Y.Z'" (quote-style insensitive)
+                return line.replace('"', "'").split("=")[-1].split("'")[1]
+    raise Exception("cannot find __version__")
+
+
+def _get_pypi_requirements() -> List[str]:
+    return [m.replace("==", ">=") for m in REQUIREMENTS if "git+" not in m]
+
+
+def _get_git_requirements() -> List[str]:
+    def valid(req: str) -> bool:
+        pat = r"^git\+https://github\.com/\w+/\w+@(v)?\d+\.\d+\.\d+#egg=\w+$"
+        if not re.match(pat, req):
+            raise Exception(
+                f"from {REQUIREMENTS_PATH}: "
+                f"pip-install git-package url is not in standardized format {pat} ({req})"
+            )
+        return True
+
+    return [m.replace("git+", "") for m in REQUIREMENTS if "git+" in m and valid(m)]
+
+
+def _python_lang_classifiers() -> List[str]:
+    """NOTE: Will still work even after the '3.9 -> 4.0'-transition."""
+
+    def int_10x_it(py_version: Tuple[int, int]) -> int:
+        return int(f"{py_version[0]}{py_version[1]}")
+
+    return [
+        f"Programming Language :: Python :: {r/10}"
+        for r in range(int_10x_it(OLDEST_PY_VERSION), int_10x_it(PY_VERSION) + 1)
+    ]
+
+
+def _development_status() -> str:
+    # "Development Status :: 1 - Planning",
+    # "Development Status :: 2 - Pre-Alpha",
+    # "Development Status :: 3 - Alpha",
+    # "Development Status :: 4 - Beta",
+    # "Development Status :: 5 - Production/Stable",
+    # "Development Status :: 6 - Mature",
+    # "Development Status :: 7 - Inactive",
+    version = _get_version()
+    if version.startswith("0.0.0"):
+        return "Development Status :: 2 - Pre-Alpha"
+    elif version.startswith("0.0."):
+        return "Development Status :: 3 - Alpha"
+    elif version.startswith("0."):
+        return "Development Status :: 4 - Beta"
+    elif int(version.split(".")[0]) >= 1:
+        return "Development Status :: 5 - Production/Stable"
+    else:
+        raise Exception(f"Could not figure Development Status for version: {version}")
+
+
+# Setup --------------------------------------------------------------------------------
 
 setup(
-    name='file_catalog',
-    version=file_catalog.__version__,
-    description='File catalog',
-    long_description=long_description,
-    url='https://github.com/WIPACrepo/file_catalog',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='file catalog',
-    packages=['file_catalog', 'file_catalog.schema'],
-    install_requires=install_requires,
-    package_data={
-        'file_catalog': ['data/www/*', 'data/www_templates/*', 'py.typed'],
-    },
-    entry_points={
-        'console_scripts':[
-            'file_catalog=file_catalog.__main__:main',]
-    }
+    name=NAME,
+    version=_get_version(),
+    description="File Catalog",
+    long_description=open(os.path.join(HERE, "README.md")).read(),  # include new-lines
+    long_description_content_type="text/markdown",
+    url="https://github.com/WIPACrepo/file_catalog",
+    license="MIT",
+    classifiers=sorted(
+        _python_lang_classifiers()
+        + [_development_status()]
+        + ["License :: OSI Approved :: MIT License"]
+    ),
+    keywords="file catalog",
+    packages=[NAME, f"{NAME}.schema"],
+    install_requires=_get_pypi_requirements(),
+    dependency_links=_get_git_requirements(),
+    package_data={NAME: ["data/www/*", "data/www_templates/*", "py.typed"]},
+    entry_points={"console_scripts": ["file_catalog=file_catalog.__main__:main"]},
 )

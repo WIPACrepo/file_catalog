86771034ed2a020b95c2f555500cfa1788c8525e
David Schultz <davids24@gmail.com>
2021-04-06 12:21:03 -0500

    fix archive query profile skipping

--- resources/profile_queries.py
+++ resources/profile_queries.py
@@ -29,7 +29,7 @@ for query in ret:
     try:
         if 'find' in query['command'] and query['command']['find'] == 'collections':
             continue
-        if 'filter' in query['command'] and query['command']['filter'] == {'locations.archive': None}:
+        if 'filter' in query['command'] and query['command']['filter'] == {'locations.archive': True}:
             continue # exclude unrealistic test query
         if 'planSummary' not in query:
             print(query)

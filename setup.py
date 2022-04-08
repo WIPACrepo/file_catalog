c00b29a110a48f7df642b82adba56c7508d543e5
Ric Evans <emejqz@gmail.com>
2022-04-08 17:39:18 -0500

    add to `options.package_data`

--- setup.py
+++ setup.py
@@ -4,8 +4,4 @@
 
 from setuptools import setup  # type: ignore[import]
 
-setup(
-    # TODO
-    # package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
-    entry_points={"console_scripts": ["file_catalog=file_catalog.__main__:main"]},
-)
+setup()

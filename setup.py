f4485f7ebe59f7be481f88dcf7a2604f1b885f4d
Ric Evans <emejqz@gmail.com>
2022-04-08 17:15:11 -0500

    move reqs to `setup.cfg`

--- setup.py
+++ setup.py
@@ -2,27 +2,10 @@
 """Setup."""
 
 
-import os
-import subprocess
-
 from setuptools import setup  # type: ignore[import]
 
-subprocess.run(
-    "pip install git+https://github.com/WIPACrepo/wipac-dev-tools.git".split(),
-    check=True,
-)
-from wipac_dev_tools import SetupShop  # noqa: E402  # pylint: disable=C0413
-
-shop = SetupShop(
-    "file_catalog",
-    os.path.abspath(os.path.dirname(__file__)),
-    ((3, 6), (3, 8)),
-    "File Catalog",
-)
-
 setup(
-    url="https://github.com/WIPACrepo/file_catalog",
-    package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
-    **shop.get_kwargs(subpackages=["schema"]),
+    # TODO
+    # package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
     entry_points={"console_scripts": ["file_catalog=file_catalog.__main__:main"]},
 )

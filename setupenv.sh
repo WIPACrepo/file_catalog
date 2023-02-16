c51dee250ce82a3b2bfddffd8ecad655f7742543
Patrick Meade <blinkdog@protonmail.com>
2023-02-16 17:34:39 -0600

    Install dev requirements as an extra

--- setupenv.sh
+++ setupenv.sh
@@ -4,4 +4,4 @@ python3 -m venv env
 echo "unset PYTHONPATH" >> env/bin/activate
 source env/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt -r requirements-dev.txt
+pip install .[dev]
